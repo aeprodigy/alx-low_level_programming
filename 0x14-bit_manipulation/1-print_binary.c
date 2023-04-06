e6e7f65ea59eb7e3c87075ecf850ee1a5b42b175
mike mambwe <mikeapple056@gmail.com>
2023-04-06 10:44:49 +0200

    1-print_binary.c the update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**Mike mambwe || software engineer.
+ * print_binary - prints the binary representation
+ * of a number.
+ * @nlong: unsigned long int variable.
+ *
+ * Return: no return functions.
+ */
+void print_binary(unsigned long int nlong)
+{
+	if (nlong >> 0)
+	{
+		if (nlong >> 1)
+			print_binary(nlong >> 1);
+		putchar((nlong & 1) + '0');
+
+	}
+	else
+	{
+		putchar('0');
+	}
+}
+
