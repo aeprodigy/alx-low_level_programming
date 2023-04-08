efbeea152984c60578a97663941b08f44762cfcd
mike mambwe <mikeapple056@gmail.com>
2023-04-08 14:57:34 +0200

    thee fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,16 +10,16 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int firstd;
+	int fd;
 	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	firstd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (firstd == -1)
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
